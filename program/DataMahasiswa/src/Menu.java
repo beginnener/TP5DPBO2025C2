14e0426bfbaa5a487c337535f31f9b01fb3254b9
nenematoda <natashadind@upi.edu>
2025-03-26 14:06:49 +0700

    code fin - add beberapa fungsi baru buat rapihin kode

--- program/DataMahasiswa/src/Menu.java
+++ program/DataMahasiswa/src/Menu.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Menu extends JFrame{
@@ -20,6 +22,7 @@ public class Menu extends JFrame{
 
     private int selectedIndex = -1;                             // index baris yang diklik (di set -1 karena ada index 0)
     private ArrayList<Mahasiswa> listMahasiswa;                 // list untuk menampung semua mahasiswa
+    private Database database;                                  // atribut database
 
     private JPanel mainPanel;
     private JTextField nimField;
@@ -34,18 +37,25 @@ public class Menu extends JFrame{
     private JLabel namaLabel;
     private JLabel jenisKelaminLabel;
     private JRadioButton aktifRadioButton, cutiRadioButton, lulusRadioButton;
-    private String status;
+    private String selectedStatus;
+    private String nimLama;
+    private javax.swing.ButtonGroup statusButton = new javax.swing.ButtonGroup();
 
     // constructor
     public Menu() {
         listMahasiswa = new ArrayList<>();                                          // inisialisasi listMahasiswa
-        populateList();                                                             // isi listMahasiswa
+        database = new Database();                                                  // populatelist() digantikan dengan objek database
         mahasiswaTable.setModel(setTable());                                        // isi GUI tabel mahasiswa
         titleLabel.setFont(titleLabel.getFont().deriveFont(Font.BOLD, 20f));    // ubah styling title
 
         // atur combo box
         String[] jenisKelaminData = {"", "Laki-laki", "Perempuan"};                  // atur isi combo box
         jenisKelaminComboBox.setModel(new DefaultComboBoxModel(jenisKelaminData));
+
+        // deklarasikan radio button kedalam grup
+        statusButton.add(aktifRadioButton);
+        statusButton.add(cutiRadioButton);
+        statusButton.add(lulusRadioButton);
 
         // sembunyikan button delete
         deleteButton.setVisible(false);
@@ -61,6 +71,7 @@ public class Menu extends JFrame{
                 }
             }
         });
+
         // saat tombol delete ditekan
         deleteButton.addActionListener(new ActionListener() {
             @Override
@@ -78,6 +89,7 @@ public class Menu extends JFrame{
                 }
             }
         });
+
         // saat tombol cancel ditekan
         cancelButton.addActionListener(new ActionListener() {
             @Override
@@ -85,12 +97,13 @@ public class Menu extends JFrame{
                 clearForm();
             }
         });
+
         // buat listener untuk radio button
         ActionListener radioListener = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JRadioButton selectedButton = (JRadioButton) e.getSource();
-                status = selectedButton.getText();
+                selectedStatus = selectedButton.getText();
             }
         };
 
@@ -110,13 +123,17 @@ public class Menu extends JFrame{
                 String selectedNim = mahasiswaTable.getModel().getValueAt(selectedIndex, 1).toString();
                 String selectedNama = mahasiswaTable.getModel().getValueAt(selectedIndex, 2).toString();
                 String selectedJenisKelamin = mahasiswaTable.getModel().getValueAt(selectedIndex, 3).toString();
-                String status = mahasiswaTable.getModel().getValueAt(selectedIndex, 4).toString();
+                String selectedStatus = mahasiswaTable.getModel().getValueAt(selectedIndex, 4).toString();
+
+                // store nim sebelumnya ke nim lama (utk handle kalau ada yang mau ubah nimnya, misal nimnya salah)
+                nimLama = selectedNim;
 
                 // ubah isi textfield dan combo box
                 nimField.setText(selectedNim);
+//                nimField.setEditable(false);                                            // jika masuk ke mode ketika baris pada tabel dipilih nim tidak bisa diubah
                 namaField.setText(selectedNama);
                 jenisKelaminComboBox.setSelectedItem(selectedJenisKelamin);
-                selectRadioButton(status);
+                selectRadioButton(selectedStatus);
 
                 // ubah button "Add" menjadi "Update"
                 addUpdateButton.setText("Update");
@@ -133,18 +150,29 @@ public class Menu extends JFrame{
         // buat objek tabel dengan kolom yang sudah dibuat
         DefaultTableModel temp = new DefaultTableModel(null, column);
 
-        // isi tabel dengan listMahasiswa
-        for(int i = 0; i < listMahasiswa.size(); i++){
-            Object[] row = new Object[5];
-            row[0] = i + 1;
-            row[1] = listMahasiswa.get(i).getNim();
-            row[2] = listMahasiswa.get(i).getNama();
-            row[3] = listMahasiswa.get(i).getJenisKelamin();
-            row[4] = listMahasiswa.get(i).getStatusMahasiswa();
+        try{
+            ResultSet resultSet = database.selectQuery("SELECT * FROM mahasiswa");
+            if(!resultSet.next()){
+                System.out.println("table kosong");
+                return null;
+            }
 
-            temp.addRow(row);
+            // isi tabel dengan resultset
+            int i = 0;
+            while(resultSet.next()){
+                Object[] row = new Object[5];
+                row[0] = i + 1;
+                row[1] = resultSet.getString("nim");
+                row[2] = resultSet.getString("nama");
+                row[3] = resultSet.getString("jenis_kelamin");
+                row[4] = resultSet.getString("status");
+
+                temp.addRow(row);
+                i++;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-
         return temp; // return juga harus diganti
     }
 
@@ -154,32 +182,32 @@ public class Menu extends JFrame{
         String nama = namaField.getText();
         String jeniskelamin = jenisKelaminComboBox.getSelectedItem().toString();
 
-        if (aktifRadioButton.isSelected()) {
-            status = "Aktif";
-        } else if (cutiRadioButton.isSelected()) {
-            status = "Cuti";
-        } else if (lulusRadioButton.isSelected()) {
-            status = "Lulus";
+        // utk menangani input radio button
+        String status = statusListener();
+
+        // Pastikan input tidak kosong
+        nullChecker(nim, nama, jeniskelamin, status);
+
+        if(!checkNimAvailability(nim)){
+            // tambahkan data kedalam database
+            String sql = "INSERT INTO mahasiswa VAlUES (null, '" + nim + "', '" + nama + "', '" + jeniskelamin + "', '" + status + "');";
+            database.insertUpdateDeleteQuery(sql);
+
+            // feedback
+            System.out.println("Insert berhasil!");
+            JOptionPane.showMessageDialog(null, "Data berhasil ditambahkan");
+
+            // update tabel
+            mahasiswaTable.setModel(setTable());
+
+            // bersihkan form
+            clearForm();
+        }
+        else{
+            System.out.print("insert gagal cek kembali input data.");
+            JOptionPane.showMessageDialog(null, "NIM sudah tersedia, mohon masukan kombinasi NIM lainnya.");
         }
 
-        // Pastikan status tidak kosong
-        if (status.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Pilih status terlebih dahulu!");
-            return;
-        }
-
-        // tambahkan data ke dalam list
-        listMahasiswa.add(new Mahasiswa(nim, nama, jeniskelamin, status));
-
-        // update tabel
-        mahasiswaTable.setModel(setTable());
-
-        // bersihkan form
-        clearForm();
-
-        // feedback
-        System.out.println("Insert berhasil!");
-        JOptionPane.showMessageDialog(null, "Data berhasil ditambahkan");
     }
 
     public void updateData() {
@@ -188,41 +216,41 @@ public class Menu extends JFrame{
         String nama = namaField.getText();
         String jeniskelamin = jenisKelaminComboBox.getSelectedItem().toString();
 
-        if (aktifRadioButton.isSelected()) {
-            status = "Aktif";
-        } else if (cutiRadioButton.isSelected()) {
-            status = "Cuti";
-        } else if (lulusRadioButton.isSelected()) {
-            status = "Lulus";
+        // utk menangani input radio button
+        String status = statusListener();
+
+        // Pastikan input tidak kosong
+        nullChecker(nim, nama, jeniskelamin, status);
+
+        // ubah data mahasiswa di database
+        if(nim.equals(nimLama) || !checkNimAvailability(nim)){                                          // cek apabila nim baru sudah ada atau belum di database
+            String sql = "UPDATE mahasiswa SET nim = '" + nim + "', " +
+                    "                          nama = '" + nama + "', " +
+                    "                          jenis_kelamin = '" + jeniskelamin + "', " +
+                    "                          status = '" + status +"' " +
+                    "                          WHERE nim = '" + nimLama + "';";
+            database.insertUpdateDeleteQuery(sql);
+            // update tabel
+            mahasiswaTable.setModel(setTable());
+            // bersihkan form
+            clearForm();
+
+            // feedback
+            System.out.println("Update berhasil!");
+            JOptionPane.showMessageDialog(null, "Data berhasil diubah");
         }
-
-        // Pastikan status tidak kosong
-        if (status.isEmpty()) {
-            JOptionPane.showMessageDialog(null, "Pilih status terlebih dahulu!");
-            return;
+        else{
+            System.out.print("insert gagal cek kembali input data.");
+            JOptionPane.showMessageDialog(null, "NIM sudah tersedia, mohon masukan kombinasi NIM lainnya.");
         }
-
-        // ubah data mahasiswa di list
-        listMahasiswa.get(selectedIndex).setNim(nim);
-        listMahasiswa.get(selectedIndex).setNama(nama);
-        listMahasiswa.get(selectedIndex).setJenisKelamin(jeniskelamin);
-        listMahasiswa.get(selectedIndex).setStatusMahasiswa(status);
-
-        // update tabel
-        mahasiswaTable.setModel(setTable());
-
-        // bersihkan form
-        clearForm();
-
-        // feedback
-        System.out.println("Update berhasil!");
-        JOptionPane.showMessageDialog(null, "Data berhasil diubah");
 
     }
 
     public void deleteData() {
         // hapus data dari list
-        listMahasiswa.remove(selectedIndex);
+        String nim = nimField.getText();
+        String sql = "DELETE FROM mahasiswa WHERE nim = '" + nim + "';";
+        database.insertUpdateDeleteQuery(sql);
 
         // update tabel
         mahasiswaTable.setModel(setTable());
@@ -246,40 +274,54 @@ public class Menu extends JFrame{
         // sembunyikan button delete
         deleteButton.setVisible(false);
 
+        // atur radio button menjadi kosong
+        statusButton.clearSelection();
+
         // ubah selectedIndex menjadi -1 (tidak ada baris yang dipilih)
         selectedIndex = -1;
+
+        System.out.println("form dibersihkan.");
     }
 
-    private void selectRadioButton(String status) {
-        if (status.equals("Aktif")) {
-            aktifRadioButton.setSelected(true);
-        } else if (status.equals("Cuti")) {
-            cutiRadioButton.setSelected(true);
-        } else if (status.equals("Lulus")) {
-            lulusRadioButton.setSelected(true);
+    private void nullChecker(String nim, String nama, String jeniskelamin, String status){
+        if (nim.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "NIM harus diisi!");
+            return;
+        }if (nama.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Nama harus diisi!");
+            return;
+        }if (jeniskelamin.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Pilih jenis kelamin!");
+            return;
+        }if (status.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Pilih status terlebih dahulu!");
+            return;
         }
     }
 
-    private void populateList() {
-        listMahasiswa.add(new Mahasiswa("2203999", "Amelia Zalfa Julianti", "Perempuan", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2202292", "Muhammad Iqbal Fadhilah", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2202346", "Muhammad Rifky Afandi", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2210239", "Muhammad Hanif Abdillah", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2202046", "Nurainun", "Perempuan", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2205101", "Kelvin Julian Putra", "Laki-laki", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2200163", "Rifanny Lysara Annastasya", "Perempuan", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2202869", "Revana Faliha Salma", "Perempuan", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2209489", "Rakha Dhifiargo Hariadi", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2203142", "Roshan Syalwan Nurilham", "Laki-laki", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2200311", "Raden Rahman Ismail", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2200978", "Ratu Syahirah Khairunnisa", "Perempuan", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2204509", "Muhammad Fahreza Fauzan", "Laki-laki", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2205027", "Muhammad Rizki Revandi", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2203484", "Arya Aydin Margono", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2200481", "Marvel Ravindra Dioputra", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2209889", "Muhammad Fadlul Hafiizh", "Laki-laki", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2206697", "Rifa Sania", "Perempuan", "Cuti"));
-        listMahasiswa.add(new Mahasiswa("2207260", "Imam Chalish Rafidhul Haque", "Laki-laki", "Aktif"));
-        listMahasiswa.add(new Mahasiswa("2204343", "Meiva Labibah Putri", "Perempuan", "Aktif"));
+    private void selectRadioButton(String selectedStatus) {
+        if (selectedStatus.equals("Aktif")) aktifRadioButton.setSelected(true);
+        else if (selectedStatus.equals("Cuti")) cutiRadioButton.setSelected(true);
+        else if (selectedStatus.equals("Lulus")) lulusRadioButton.setSelected(true);
+    }
+
+    private String statusListener(){
+        String status = "";
+        if (aktifRadioButton.isSelected()) status = "Aktif";
+        else if (cutiRadioButton.isSelected()) status = "Cuti";
+        else if (lulusRadioButton.isSelected()) status = "Lulus";
+        return status;
+    }
+
+    // untuk mengecek apakah nim sudah ada atau belum
+    private boolean checkNimAvailability(String nim){
+        String sql = "SELECT COUNT(*) FROM mahasiswa where nim = '" + nim + "'";
+        try{
+            ResultSet rs = database.checkNimExist(sql);
+            if (rs.next()) return rs.getInt(1) > 0;                             // jika ada hasil yang lebih dari 0 berarti nim sudah ada dan fungsi akan mereturn 1 atau true
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return false;                                                                // jika nim belum ada fungsi akan mereturn 0 atau false
     }
 }
